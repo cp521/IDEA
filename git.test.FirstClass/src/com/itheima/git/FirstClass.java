9a5d3948f868f9167c3893bc10d96cfe33da8436
wenjun <wenjun>
2019-10-15 22:27:36 +0800

    修改FirstClass,添加注释hello

--- git.test.FirstClass/src/com/itheima/git/FirstClass.java
+++ git.test.FirstClass/src/com/itheima/git/FirstClass.java
@@ -7,10 +7,12 @@ public class FirstClass {
     System.out.println("Hello git");
     System.out.println("Hello git");
     System.out.println("Hello git");
+    //hello
   }
 
   public void test(){
     int i = 0;
-
+    i+=10;
+    System.out.println(i);
   }
 }
